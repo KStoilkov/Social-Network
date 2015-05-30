7bc7328d54c2e19c2aaeacedcded27d4a724b819
Kiril Stoilkov <K.I.Stoilkov@gmail.com>
2015-05-31 01:16:16 +0300

    Comment like/unlike functionality added

--- app/js/controllers/user/CommentController.js
+++ app/js/controllers/user/CommentController.js
@@ -47,19 +47,43 @@ app.controller('CommentController', function ($scope, commentService) {
                 console.log(err);
             }
         );
-        
-        $scope.showAllComments = function (postId) {
-            $scope.comments = angular.copy($scope.allComments);
-            //$scope.getPostComments(postId);
-            $scope.allCommentsShowed = true;
-            $scope.textAfterComments = 'Show less comments..';
-        };
+    };
 
-        $scope.showLessComments = function (postId) {
-            $scope.comments = angular.copy($scope.displayComments);
-            //$scope.getPostComments(postId);
-            $scope.allCommentsShowed = false;
-            $scope.textAfterComments = 'Show more comments..';
-        };
+    $scope.showAllComments = function () {
+        $scope.comments = angular.copy($scope.allComments);
+        $scope.allCommentsShowed = true;
+        $scope.textAfterComments = 'Show less comments..';
+    };
+
+    $scope.showLessComments = function () {
+        $scope.comments = angular.copy($scope.displayComments);
+        $scope.allCommentsShowed = false;
+        $scope.textAfterComments = 'Show more comments..';
+    };
+
+    $scope.likeComment = function (postId, commentId) {
+        commentService.likeComment(
+            postId,
+            commentId,
+            function () {
+                $scope.getPostComments(postId);
+            },
+            function (err) {
+                console.log(err);
+            }
+        );
+    };
+
+    $scope.unlikeComment = function (postId, commentId) {
+        commentService.unlikeComment(
+            postId,
+            commentId,
+            function () {
+                $scope.getPostComments(postId);
+            },
+            function (err) {
+                console.log(err);
+            }
+        );
     };
 });
--- app/js/services/commentService.js
+++ app/js/services/commentService.js
@@ -26,8 +26,34 @@ app.factory('commentService', function ($http, baseServiceUrl, authService) {
         $http(request).success(success).error(error);
     };
 
+    function likeComment(postId, commentId, success, error) {
+        var request = {
+            method: 'POST',
+            url: baseServiceUrl + 'posts/' + postId + '/comments/' + commentId + '/likes',
+            headers: {
+                'Authorization' : authService.getUserAuthorization()
+            }
+        };
+
+        $http(request).success(success).error(error);
+    };
+
+    function unlikeComment(postId, commentId, success, error) {
+        var request = {
+            method: 'DELETE',
+            url: baseServiceUrl + 'posts/' + postId + '/comments/' + commentId + '/likes',
+            headers: {
+                'Authorization' : authService.getUserAuthorization()
+            }
+        };
+
+        $http(request).success(success).error(error);
+    };
+
     return {
         getPostComments : getPostComments,
-        getCommentLikes : getCommentLikes
+        getCommentLikes : getCommentLikes,
+        likeComment : likeComment,
+        unlikeComment : unlikeComment
     }
 });
